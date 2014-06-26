93b3a599fa22daf9a5023569a494cfe5304cb12c
otakuchiyan <codeotakuchiyan@gmail.com>
2014-06-26 22:06:47 +0000

    Backup

---
+++ CMakeFiles/tinyterm.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/kirino/tinyterm/src/tinyterm.c" "/home/kirino/tinyterm/CMakeFiles/tinyterm.dir/src/tinyterm.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/vte-0.0"
+  "/usr/include/gtk-2.0"
+  "/usr/lib/gtk-2.0/include"
+  "/usr/include/pango-1.0"
+  "/usr/include/atk-1.0"
+  "/usr/include/gdk-pixbuf-2.0"
+  "/usr/include/libpng16"
+  "/usr/include/gio-unix-2.0"
+  "/usr/include/cairo"
+  "/usr/include/glib-2.0"
+  "/usr/lib/glib-2.0/include"
+  "/usr/include/pixman-1"
+  "/usr/include/freetype2"
+  "/usr/include/harfbuzz"
+  "/usr/include/libdrm"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/tinyterm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyterm.dir/src/tinyterm.c.o"
+  "tinyterm.pdb"
+  "tinyterm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tinyterm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
